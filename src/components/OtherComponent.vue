f153b434b9d60d5a66d931ab35e43608e1682b2a
leekangtaqi <leekangtaqi@sina.com>
2017-02-23 16:32:35 +0800

    init

---
+++ src/components/OtherComponent.vue
@@ -0,0 +1,41 @@
+<div>
+
+	<div v-on:click="msg += 'xxxxxx'">
+		<div>
+			<div>{{ msg }}</div>
+		</div>
+	</div>
+	
+</div>
+
+<script>	
+
+	export default {
+		name: 'other-component',
+		data () {
+			return {
+				msg: 'testing !',
+				msg1: 'crashed !',
+				dynamicId: true
+			}
+		},
+		created: function() {
+			this.msg = '2222';
+			this.msg1 = 'fffff'
+		},
+		watch: {
+			msg: function(val) {
+				console.warn('watched' + val);
+			}
+		},
+		methods: {
+			xxx: function(e) {
+				alert(this.msg)
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,30 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import Vuex from 'vuex';
+
+console.warn(typeof Vuex);
+
+Vue.use(Vuex);
+
+Vue.config.errorHandler = function(e) {
+  console.warn(e);
+}
+
+Vue.directive('focus', {
+  // 当绑定元素插入到 DOM 中。
+  inserted: function (el) {
+    // 聚焦元素
+    el.focus()
+  }
+})
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
